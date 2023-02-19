0f1ca517e0fbf203b6cdf390740dc2b88df4707c
code-with-adhi <125806079+code-with-adhi@users.noreply.github.com>
2023-02-19 19:14:03 +0530

    array subtraction.

---
+++ array-diff.py
@@ -0,0 +1,8 @@
+firstArray = [1,2,2,2,3]
+print(firstArray.count(2))
+# secondArray = [1,2]
+# for item in secondArray:
+#     if item in firstArray:
+#         while(firstArray.count(item)):
+#             firstArray.remove(item)
+# print(firstArray)
